837a706b668db7d4b8e6a31cefeb25e672121637
Nikolai <micurino@gmail.com>
2025-09-23 13:02:30 -0700

    add capacitor integration with android build

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.nikuz.ereader',
+  appName: 'e-reader',
+  webDir: 'dist'
+};
+
+export default config;
