5737b667f3211d2a64f75d63c39f0d6fe367bc77
Nikolai <micurino@gmail.com>
2025-12-02 16:47:38 -0800

    update app name and icon

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# e-reader
+# My Reader
 
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
     appId: 'com.nikuz.ereader',
-    appName: 'e-reader',
+    appName: 'My Reader',
     webDir: 'dist',
     plugins: {
         CapacitorSQLite: {
